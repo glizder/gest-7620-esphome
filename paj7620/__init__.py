fca078656a4807253b73f2bf7cceaa62a2cba988
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 18:17:22 +0200

    Add files via upload

---
+++ paj7620/__init__.py
@@ -0,0 +1,22 @@
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import CONF_ID, CONF_NAME
+
+paj7620_ns = cg.esphome_ns.namespace("paj7620")
+PAJ7620Component = paj7620_ns.class_("PAJ7620Component", cg.Component)
+
+CONF_GESTURE = "gesture"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(PAJ7620Component),
+    cv.Required(CONF_GESTURE): text_sensor.text_sensor_schema(CONF_NAME),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_PAJ7620Component()
+    await cg.register_component(var, config)
+
+    gesture_sensor = await text_sensor.new_text_sensor(config[CONF_GESTURE])
+    cg.add(var.set_gesture_sensor(gesture_sensor))
