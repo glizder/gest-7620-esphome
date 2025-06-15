96db1eef79726331efa13e6e51a8830981d9ceb5
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 21:29:19 +0200

    Update __init__.py

--- components/paj7620/__init__.py
+++ components/paj7620/__init__.py
@@ -14,7 +14,8 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = PAJ7620Component(config[CONF_ID])
+    var = cg.new_PAJ7620Component = PAJ7620Component
+    var = cg.new_PAJ7620Component(config[CONF_ID])
     await cg.register_component(var, config)
     gesture = await text_sensor.new_text_sensor(config[CONF_GESTURE])
     cg.add(var.set_gesture_sensor(gesture))
