4b00172262f49eff80d560583c9642caebeb65db
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 21:36:58 +0200

    Update __init__.py

--- components/paj7620/__init__.py
+++ components/paj7620/__init__.py
@@ -3,21 +3,24 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID, CONF_NAME
 
+# Tworzymy przestrzeń nazw i klasę komponentu
 paj7620_ns = cg.esphome_ns.namespace("paj7620")
 PAJ7620Component = paj7620_ns.class_("PAJ7620Component", cg.Component)
 
 CONF_GESTURE = "gesture"
 
+# Schemat konfiguracji komponentu
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(PAJ7620Component),
     cv.Required(CONF_GESTURE): text_sensor.text_sensor_schema(CONF_NAME),
 }).extend(cv.COMPONENT_SCHEMA)
 
+# Główna funkcja generująca kod C++
 async def to_code(config):
-    # 👇 zarejestruj zmienną (to rozwiązuje błąd z ID!)
+    # Tworzymy zmienną i wiążemy ją z ID
     var = cg.new_variable(config[CONF_ID], PAJ7620Component)
     await cg.register_component(var, config)
 
-    # 👇 dodaj sensor
+    # Tworzymy sensor tekstowy do wykrywania gestów
     gesture = await text_sensor.new_text_sensor(config[CONF_GESTURE])
     cg.add(var.set_gesture_sensor(gesture))
